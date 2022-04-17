8b6bbb8d55ec9838f19547d07259300366f9ebbf
Digx7 <37136301+Digx7@users.noreply.github.com>
2022-04-16 18:44:33 -0700

    Updated Materials

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn;
+    public Vector2 objectSpawnRangeX;
+    public Vector2 objectSpawnRangeZ;
+
+    private Vector3 trueRange;
 
     public void Start(){
       //SpawnObject(objectToSpawn);
     }
 
     public GameObject SpawnObject(GameObject obj){
-      GameObject spawnedObj = Instantiate(obj, this.transform.position, Quaternion.identity);
+      trueRange = this.transform.position;
+      trueRange.x += Random.Range(objectSpawnRangeX.x, objectSpawnRangeX.y);
+      trueRange.z+= Random.Range(objectSpawnRangeZ.x, objectSpawnRangeZ.y);
+
+      GameObject spawnedObj = Instantiate(obj, trueRange, Quaternion.identity);
       return spawnedObj;
     }
 }
